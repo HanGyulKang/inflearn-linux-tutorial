689c5bdb107c7b8fe07f92cc127a2efe3073b7a5
kanghangyul <gang6607@gmail.com>
2022-03-07 22:14:56 +0900

    2022 03 07

---
+++ src/memo/linux-memo.sh
@@ -0,0 +1,23 @@
+# mkdir [filename]
+    디렉토리 생성
+
+# rm [filename]
+    파일 삭제
+    # -r [directory]
+        디렉토리 및 하위 파일 전체 삭제
+    # rmdir
+        디렉토리 삭제
+
+# cd
+    change directory
+
+# ls
+    list segments : 목록 표시
+    # -l
+        long format, list로 표시, permission 정보 표시
+    # -a
+        "." 포함 관련 디렉토리 모든 파일 표시
+        # -al 로 합체 가능
+
+# pwd
+    현재 위치 표시
