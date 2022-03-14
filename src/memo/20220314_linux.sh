d62b1310c4850c4fceab348383e9424f74f7c772
kanghangyul <gang6607@gmail.com>
2022-03-14 23:53:12 +0900

    2022 03 14

---
+++ src/memo/20220314_linux.sh
@@ -0,0 +1,19 @@
+# 사용설명서
+  간단한 메뉴얼 확인
+  # 1. ls --help
+  전용페이지로 이동하여 상세한 메뉴얼 확인
+  # 2. man ls
+    /검색어 : slash + 검색어로 man으로 이동한 메뉴얼에서 특정 단어 검색 가능
+
+# mkdir -p --parents : 부모 이하의 자식 디렉토리까지 만들 수 있음
+  mkdir -p dir1/dir2/dir3/dir4/dir5
+  mkdir --parents dir1/dir2/dir3/dir4/dir5
+
+# ls -a
+  숨겨진 파일 또는 디렉토리는 명칭 앞에 '.' 이 붙는다.
+    # ls -a 또는 ls --all 로 숨겨진 파일까지 볼 수 있다.
+    # ls -al 또는 ls -la로 l과 합쳐서 명령을 내릴 수 있다.
+
+# ls -S
+  소문자와 대문자를 !!반드시!! 구분해야 한다.
+  sorting 명령어
